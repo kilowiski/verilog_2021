e5ecd183430f793042765d4a4480ad9dfd7713db
Jos Adhika <97659553+kilowiski@users.noreply.github.com>
2023-10-16 23:50:25 +0800

    add valid

--- divisor_tb.v
+++ divisor_tb.v
@@ -11,8 +11,9 @@ module test #(parameter WIDTH=4);
   wire [2:0] NEXT_OUT;
   wire [WIDTH-1:0] COUNT_OUT;
   wire COUNT_MAX;
+  wire valid;
   
-  divisor div0(.A_IN(A_IN),.B_IN(B_IN),.Q_OUT(Q_OUT),.R_OUT(R_OUT),.STATE_OUT(STATE_OUT),.NEXT_OUT(NEXT_OUT),.clk(clk),.rst(rst),.en(en),.A_REG(A_REG),.B_REG(B_REG),. COUNT_OUT(COUNT_OUT),.COUNT_MAX(COUNT_MAX));
+  divisor div0(.A_IN(A_IN),.B_IN(B_IN),.Q_OUT(Q_OUT),.R_OUT(R_OUT),.STATE_OUT(STATE_OUT),.NEXT_OUT(NEXT_OUT),.clk(clk),.rst(rst),.en(en),.A_REG(A_REG),.B_REG(B_REG),. COUNT_OUT(COUNT_OUT),.COUNT_MAX(COUNT_MAX),.valid(valid));
   initial begin
     $dumpfile("dump.vcd");
     $dumpvars(1);
@@ -20,15 +21,12 @@ module test #(parameter WIDTH=4);
     rst=1;
     #20
     rst=0;
-    A_IN=3;
-    B_IN=2;
+    A_IN=12;
+    B_IN=0;
     #30
     en=1;
     #10
     en=0;
-    #70
-    A_IN=13;
-    B_IN=5;
 
   end
   
